951f95f6fdf96dd5faf1cf5fdfa82be34ef8840e
Gabriel Luethje <gabriel@alley.co>
2021-03-16 16:15:31 -0700

    Add webfontloader package, load Open Sans

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import data from './data';
-import './App.css';
+import WebFontLoader from 'webfontloader'
 import { Wrapper } from  './styles';
 import Card from './components/Card';
 
 function App() {
+  WebFontLoader.load({
+    google: {
+      families: ['Open+Sans:700:latin-ext']
+    }
+  })
   return (
-    <div className="App">
-      <Wrapper>
-      {data.map(({
-        title,
-        imgUrl,
-        avatarUrl,
-        time,
-        distance,
-        moreUrl,
-        workouts,
-      }) => (
-        <Card
-          title={title}
-          imgUrl={imgUrl || 'https://placekitten.com/600/400'}
-          avatarUrl={avatarUrl}
-          time={time}
-          distance={distance}
-          moreUrl={moreUrl}
-          workouts={workouts}
-        />
-      ))}
-      </Wrapper>
-    </div>
+    <Wrapper>
+    {data.map(({
+      title,
+      imgUrl,
+      avatarUrl,
+      time,
+      distance,
+      moreUrl,
+      workouts,
+    }) => (
+      <Card
+        title={title}
+        imgUrl={imgUrl || 'https://placekitten.com/600/400'}
+        avatarUrl={avatarUrl}
+        time={time}
+        distance={distance}
+        moreUrl={moreUrl}
+        workouts={workouts}
+      />
+    ))}
+    </Wrapper>
   );
 }
 
--- src/styles.js
+++ src/styles.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   display: grid;
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 700;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 20px;
 `;
