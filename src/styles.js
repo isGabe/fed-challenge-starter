48a70799de010d1e3eedc9d0589c495fef0090c9
Gabriel Luethje <gabriel@alley.co>
2021-03-17 13:18:08 -0700

    Ensure cards are equal height, reduce line-height

--- src/styles.js
+++ src/styles.js
@@ -5,12 +5,14 @@ export const Wrapper = styled.div`
   font-family: 'Open Sans', sans-serif;
   font-weight: 700;
   letter-spacing: ${rem(0.3)};
+  line-height: 1.2;
   margin: 0 auto;
   max-width: 90vw;
   padding: 60px 0;
 
   @media (min-width: 32rem) {
     display: grid;
+    grid-auto-rows: 1fr;
     grid-template-columns: repeat(2, 1fr);
     grid-gap: ${rem(20)};
   }
