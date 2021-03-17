b253bb30ae5d98cbaec8367a6539013bfd2b83a4
Gabriel Luethje <gabriel@alley.co>
2021-03-17 13:00:11 -0700

    Updates

---
+++ precache-manifest.d308181a21edff30c35b58b073c6ed45.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39a1178e8317c7babda39aa2d26915e",
+    "url": "/fed-challenge-starter/index.html"
+  },
+  {
+    "revision": "93f9cd305cf3bf5274ab",
+    "url": "/fed-challenge-starter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f12a2055530035137580",
+    "url": "/fed-challenge-starter/static/js/2.0a1494d3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/fed-challenge-starter/static/js/2.0a1494d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93f9cd305cf3bf5274ab",
+    "url": "/fed-challenge-starter/static/js/main.c9ebdd6e.chunk.js"
+  },
+  {
+    "revision": "ddf4e5f1c600b5f0db56",
+    "url": "/fed-challenge-starter/static/js/runtime-main.3972a04a.js"
+  },
+  {
+    "revision": "3d8f3cc1e1a80889c6fd04ce3621c6cc",
+    "url": "/fed-challenge-starter/static/media/icon-distance.3d8f3cc1.svg"
+  },
+  {
+    "revision": "cf4bf91fb344ab773980b8bf0701dfae",
+    "url": "/fed-challenge-starter/static/media/icon-playlist.cf4bf91f.svg"
+  },
+  {
+    "revision": "cff9118de2443910f9b9d48c0f80dd52",
+    "url": "/fed-challenge-starter/static/media/icon-timer.cff9118d.svg"
+  }
+]);
